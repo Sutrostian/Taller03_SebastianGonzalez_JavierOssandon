35736c137db671af74498fe00c98fc997c91a554
Sebastian Gonzalez Rossi <sebastian.gonzalez06@alumnos.ucn.cl>
2026-06-07 19:13:03 -0400

    Update Final -Se agrega Diagrama de clases -Se agrega Modelo de dominio

--- src/domain/HechizoFuego.java
+++ src/domain/HechizoFuego.java
@@ -1,32 +1,32 @@
-package domain;
-
-public class HechizoFuego extends Hechizo implements ICalcularP {
-
-	private int duracionQuemadura;
-
-	public HechizoFuego(String nombreHechizo, String tipo, int dano, int duracionQuemadura) {
-		super(nombreHechizo, tipo, dano);
-		this.duracionQuemadura = duracionQuemadura;
+	package domain;
+	
+	public class HechizoFuego extends Hechizo implements ICalcularP {
+	
+		private int duracionQuemadura;
+	
+		public HechizoFuego(String nombreHechizo, String tipo, int dano, int duracionQuemadura) {
+			super(nombreHechizo, tipo, dano);
+			this.duracionQuemadura = duracionQuemadura;
+		}
+	
+		public int getDuracionQuemadura() {
+			return duracionQuemadura;
+		}
+	
+		public void setDuracionQuemadura(int duracionQuemadura) {
+			this.duracionQuemadura = duracionQuemadura;
+		}
+	
+		@Override
+		public String toString() {
+			return super.toString() + " HechizoFuego [duracionQuemadura=" + duracionQuemadura + "]";
+		}
+	
+		@Override
+		public double calcularPuntaje() {
+	
+			return dano * duracionQuemadura;
+	
+		}
+	
 	}
-
-	public int getDuracionQuemadura() {
-		return duracionQuemadura;
-	}
-
-	public void setDuracionQuemadura(int duracionQuemadura) {
-		this.duracionQuemadura = duracionQuemadura;
-	}
-
-	@Override
-	public String toString() {
-		return super.toString() + " HechizoFuego [duracionQuemadura=" + duracionQuemadura + "]";
-	}
-
-	@Override
-	public double calcularPuntaje() {
-
-		return dano * duracionQuemadura;
-
-	}
-
-}
